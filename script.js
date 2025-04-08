0411854c057900b3a7f0a6424bbb669fd03e4b25
Nisha Bukke <sononenisha827@gmail.com>
2025-04-08 10:49:15 +0530

    change

--- script.js
+++ script.js
@@ -63,21 +63,7 @@ window.onclick = function(event) {
 }
 
 
-// loader js
 
-document.onreadystatechange = function () {
-  if (document.readyState !== "complete") {
-      document.querySelector(
-          "body").style.visibility = "hidden";
-      document.querySelector(
-          "#preloader").style.visibility = "visible";
-  } else {
-      document.querySelector(
-          "#preloader").style.display = "none";
-      document.querySelector(
-          "body").style.visibility = "visible";
-  }
-  };
 
 
   function openGallery() {
